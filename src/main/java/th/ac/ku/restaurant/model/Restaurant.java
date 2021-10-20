bb3255e414717ad96b0ed1621b44a20358e99e3a
meuang <rachapol.t@ku.th>
2021-10-20 15:31:53 +0700

    API

---
+++ src/main/java/th/ac/ku/restaurant/model/Restaurant.java
@@ -0,0 +1,68 @@
+package th.ac.ku.restaurant.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class Restaurant {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(columnDefinition = "CHAR(36)")
+    private UUID id;
+
+    private String name;
+    private String address;
+    private String phone;
+    private int numSeats;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getNumSeats() {
+        return numSeats;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        if (name != null)
+            this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setNumSeats(int numSeats) {
+        this.numSeats = numSeats;
+    }
+}
